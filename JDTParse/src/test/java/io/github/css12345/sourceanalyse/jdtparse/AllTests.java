72914c998957af7dc1779e7645b72f800e5ded1f
css12345 <2318018219@qq.com>
2020-03-07 00:01:35 +0800

    test:为所有测试类创建一个启动类

---
+++ JDTParse/src/test/java/io/github/css12345/sourceanalyse/jdtparse/AllTests.java
@@ -0,0 +1,11 @@
+package io.github.css12345.sourceanalyse.jdtparse;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("io.github.css12345.sourceanalyse.jdtparse")
+public class AllTests {
+
+}
