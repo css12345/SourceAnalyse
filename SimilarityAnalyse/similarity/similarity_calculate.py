d2e625c450a75a2fee25d59da4a00dc57fe06103
css12345 <2318018219@qq.com>
2020-03-18 21:57:44 +0800

    refactor:重构相似性计算模块参数转换部分
    在calculate函数中，initialization_object可传递邻接表格式或邻接矩阵格式，邻接表为Map<Integer,List<Integer>>，key为节点ID，value为相连的节点ID列表；邻接矩阵可以是List<List<Integer>>或jep.NDArray。
    node_labels为Map<Integer,String>，key为节点ID，value为标签。
    ID可以是Integer/Long。
    
    所有List的实现必须是ArrayList或LinkedList，Map的实现必须是HashMap或LinkedHashMap或TreeMap。

--- SimilarityAnalyse/similarity/similarity_calculate.py
+++ SimilarityAnalyse/similarity/similarity_calculate.py
@@ -2,10 +2,10 @@ from grakel.graph import Graph
 from grakel.graph_kernels import GraphKernel
 
 def calculate(initialization_object1, node_labels1, initialization_object2, node_labels2):
-    initialization_object1 = convertPyJMapToDict(initialization_object1, True)
-    initialization_object2 = convertPyJMapToDict(initialization_object2, True)
-    node_labels1 = convertPyJMapToDict(node_labels1, False)
-    node_labels2 = convertPyJMapToDict(node_labels2, False)
+    initialization_object1 = transform(initialization_object1)
+    initialization_object2 = transform(initialization_object2)
+    node_labels1 = transform(node_labels1)
+    node_labels2 = transform(node_labels2)
     
     graph1 = Graph(initialization_object=initialization_object1, node_labels=node_labels1)
     graph2 = Graph(initialization_object=initialization_object2, node_labels=node_labels2)
@@ -13,14 +13,24 @@ def calculate(initialization_object1, node_labels1, initialization_object2, node
     graph_kernel.fit_transform([graph1])
     return graph_kernel.transform([graph2])[0][0]
 
-def convertPyJMapToDict(pyJMap, convertValueToList):
+def transform(obj):
+    if (hasattr(obj, "java_name") == False):
+        return obj
+    obj_java_name = getattr(obj, "java_name")
+    if (obj_java_name == "java.util.HashMap" or obj_java_name == "java.util.LinkedHashMap" or obj_java_name == "java.util.TreeMap"):
+        return convertPyJMapToDict(obj)
+    if (obj_java_name == "java.util.ArrayList" or obj_java_name == "java.util.LinkedList"):
+        return convertPyJListToList(obj)
+    return obj
+
+def convertPyJMapToDict(pyJMap):
     dictory = {}
     
     for entry in pyJMap.entrySet():
         key = entry.getKey()
         value = entry.getValue()
-        if (convertValueToList):
-            value = convertPyJListToList(value)
+        key = transform(key)
+        value = transform(value)
         dictory[key] = value
         
     return dictory
@@ -28,5 +38,6 @@ def convertPyJMapToDict(pyJMap, convertValueToList):
 def convertPyJListToList(pyJList):
     list = []
     for element in pyJList:
+        element = transform(element)
         list.append(element)
     return list
--- SimilarityAnalyse/src/test/java/io/github/css12345/sourceanalyse/similarityanalyse/TestJep.java
+++ SimilarityAnalyse/src/test/java/io/github/css12345/sourceanalyse/similarityanalyse/TestJep.java
@@ -4,13 +4,17 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.junit.jupiter.api.Test;
 
 import jep.Interpreter;
 import jep.JepException;
+import jep.NDArray;
 import jep.SharedInterpreter;
 
 public class TestJep {
@@ -26,13 +30,13 @@ public class TestJep {
 			interp.exec(addSimilarityModulePathCommand);
 			interp.exec("from similarity_calculate import calculate");
 
-			Map<Integer, List<Integer>> initializationObject1 = new HashMap<>();
-			Map<Integer, String> nodeLabels1 = new HashMap<>();
+			Map<Integer, List<Integer>> initializationObject1 = new TreeMap<>();
+			Map<Integer, String> nodeLabels1 = new LinkedHashMap<>();
 
 			Map<Integer, List<Integer>> initializationObject2 = new HashMap<>();
 			Map<Integer, String> nodeLabels2 = new HashMap<>();
 
-			initializationObject1.put(1, new ArrayList<>(Arrays.asList(2, 3)));
+			initializationObject1.put(1, new LinkedList<>(Arrays.asList(2, 3)));
 			initializationObject1.put(2, new ArrayList<>(Arrays.asList(1)));
 			initializationObject1.put(3, new ArrayList<>(Arrays.asList(1)));
 			nodeLabels1.put(1, "O");
@@ -50,6 +54,26 @@ public class TestJep {
 
 			double result = (double) interp.invoke("calculate", initializationObject1, nodeLabels1,
 					initializationObject2, nodeLabels2);
+			System.out.println(result);
+			
+			NDArray<int[]> edges1 = new NDArray<int[]>(new int[] {0,1,1,1,0,0,1,0,0}, 3,3);
+			nodeLabels1.clear();
+			nodeLabels1.put(0, "O");
+			nodeLabels1.put(1, "H");
+			nodeLabels1.put(2, "H");
+			
+			List<List<Integer>> edges2 = new ArrayList<>();
+			edges2.add(new ArrayList<>(Arrays.asList(0,1,1,1)));
+			edges2.add(new ArrayList<>(Arrays.asList(1,0,0,0)));
+			edges2.add(new ArrayList<>(Arrays.asList(1,0,0,0)));
+			edges2.add(new ArrayList<>(Arrays.asList(1,0,0,0)));
+			nodeLabels2.clear();
+			nodeLabels2.put(0, "O");
+			nodeLabels2.put(1, "H");
+			nodeLabels2.put(2, "H");
+			nodeLabels2.put(3, "H");
+			result = (double) interp.invoke("calculate", edges1, nodeLabels1,
+					edges2, nodeLabels2);
 			System.out.println(result);
 		}
 	}
